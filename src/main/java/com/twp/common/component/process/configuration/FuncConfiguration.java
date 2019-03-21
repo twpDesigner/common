2711809af0b2440b76f7c656eb22bf2399d66a66
tanweiping <18651803691@163.com>
2019-03-21 17:07:42 +0800

    cloudgateway 完成

--- src/main/java/com/twp/common/component/process/configuration/FuncConfiguration.java
+++ src/main/java/com/twp/common/component/process/configuration/FuncConfiguration.java
@@ -2,10 +2,9 @@ package com.twp.common.component.process.configuration;
 
 import com.twp.common.component.process.exception.combo.impl.ExecptionForFunc;
 import com.twp.common.component.process.exception.combo.impl.ExecptionForTFunc;
-import com.twp.common.utils.SpringContextUtil;
+import com.twp.common.utils.SpringContext;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Created by tanweiping on 2018/7/4.
@@ -14,7 +13,7 @@ import org.springframework.context.annotation.Import;
         {
                 ExecptionForFunc.class,
                 ExecptionForTFunc.class,
-                SpringContextUtil.class
+                SpringContext.class
         }
 )
 @Configuration
